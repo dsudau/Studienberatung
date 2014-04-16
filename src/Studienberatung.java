b5e35982fbf8147f86ee19ddf04d4d835a34182a
Daniel Sudau <daniel.sudau@gmail.com>
2014-04-16 12:10:33 +0200

    initialize scanner an sysout: 'hast du abitur'

--- src/Studienberatung.java
+++ src/Studienberatung.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 
 public class Studienberatung {
 	public static void main(String[] args) {
-
-	}
+		Scanner scanner = new Scanner(System.in);
+		String antwort = "";
+		
+		System.out.println("Hast du Abitur[j/n]?");
+		antwort = scanner.next();
 }
